612cf79d3313595c453b8fdb5e76c7793df5a575
jim161 <jonas.mendezreneau1@louisiana.edu>
2017-11-01 16:13:27 -0500

    added date and included missing day

--- process_netCDF_student.R
+++ process_netCDF_student.R
@@ -15,26 +15,26 @@ latmin <- 17 #right western coordinate
 
 # identify the variable you want to extract data for
 
-ncin <- nc_open(f)#open Netcdf
+data <- nc_open(file)#open Netcdf
 
-var <- ncvar_get(ncin, "chlor_a")
+var <- ncvar_get(data, "chlor_a")
 
 
 #list netCDF files
 f <- list.files(pattern=".nc",full.names=F) 
 #What pattern can you use to identify all the netCDF files?
 
-d <- plyr::adply(f, 1, function(file) {
+d <- plyr::adply(file, 1, function(file) {
 
   # open netCDF file
-  data<-nc_open(file)
+  data<-nc_open(f)
 
   # extract data
   lon<-ncvar_get(data,"lon")
   lat<- ncvar_get(data,"lat")
   tmp.array <- ncvar_get(data, data$var[[var]])
   dunits <- ncatt_get(data, var, "units")$value
-  fillvalue <- ""
+  fillvalue <- "NA"
 
   dim(tmp.array)
 
@@ -52,13 +52,19 @@ d <- plyr::adply(f, 1, function(file) {
   dateini<-ncatt_get(data,0,"time_coverage_start")$value
   dateend<-ncatt_get(data,0,"time_coverage_end")$value
   datemean<-mean(c(as.Date(dateend,"%Y-%m-%dT%H:%M:%OSZ"),as.Date(dateini,"%Y-%m-%dT%H:%M:%OSZ")))
-  year <-  # get the year
-  month <-  # get the month
-  day - # get the day
+  
+  year <- format(as.Date(datemean,"%Y-%m-%dT%H:%M:%OSZ"), "%Y" ) # get the year
+  month <- format(as.Date(datemean,"%Y-%m-%dT%H:%M:%OSZ"), "%m") # get the month
+  day <-  format(as.Date(datemean,"%Y-%m-%dT%H:%M:%OSZ"), "%dT") # get the day
 
    # prepare final data set. Include the day (it is missing in the code below)
-  dat.varSA<-data.frame(rep(as.integer(year,nrow(dat.varSAtmp))),rep(as.integer(month,nrow(dat.varSAtmp))), dat.varSAtmp, rep(dunits,nrow(dat.varSAtmp)), rep(var, nrow(dat.varSAtmp)))
+  dat.varSA<-data.frame(rep(as.integer(year,nrow(dat.varSAtmp))),
+                        rep(as.integer(month,nrow(dat.varSAtmp))), 
+                        rep(as.integer(day,nrow(dat.varSAtmp))),
+                        rep(dunits,nrow(dat.varSAtmp)), 
+                        rep(var, nrow(dat.varSAtmp)))
   names(dat.varSA)<-c("year","month","day","lon","value","unit","var")
+  
 
   # close connection
   nc_close(data)
